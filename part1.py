a8c5283c7eaebd6ae3e03bf5d2bad4c457cfda2e
Zoe Jalkut <103280964+izzymariej@users.noreply.github.com>
2022-05-13 18:00:49 +0000

    started assignment

--- part1.py
+++ part1.py
@@ -19,9 +19,13 @@ number = int(input("Enter a number: "))
 
 smallest = number
 
-number = int(input("Enter another number: "))
+number2 = int(input("Enter another number: "))
+
+number = int(input("Enter the last number: "))
 
 if number < smallest:
   smallest = number
+elif number2 < smallest:
+  smallest = number2
   
 print("The smallest number is ", smallest)
